01f86fa2c95997ad1d021db5e7393033323a4a12
mrwanaliws <mrwanali257@gmail.com>
2026-06-17 02:42:45 -0700

    Fix header navigation

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,27 +1,40 @@
 "use client"
-import Link from 'next/link'
-import React from 'react'
-import { usePathname } from 'next/navigation'
+
+import Link from "next/link"
+import { usePathname } from "next/navigation"
 
 const nav = [
   { href: "/", label: "Home" },
-  { href: "/#services", label: "services" },
-{ href: "/contact", label: "Contact" },
+  { href: "/#services", label: "Services" },
+  { href: "/contact", label: "Contact" },
 ]
 
 export default function Header() {
-  const pathname = usePathname() || '/'
+  const pathname = usePathname() || "/"
+
   return (
-    <header className="py-4 border-b border-slate-800 sticky top-0 z-30 backdrop-filter backdrop-blur-sm bg-black/30">
+    <header className="py-4 border-b border-slate-800 sticky top-0 z-30 backdrop-blur-sm bg-black/30">
       <div className="site-container flex items-center justify-between">
-        <Link href="/" className="font-bold text-xl">Marwan Ali Mohamed</Link>
+        <Link href="/" className="font-bold text-xl">
+          Marwan Ali Mohamed
+        </Link>
+
         <nav>
           <ul className="flex gap-6 items-center">
             {nav.map((n) => {
-              const isActive = pathname === n.href || (n.href === '/#services' && pathname === '/')
+              const isActive =
+                pathname === n.href ||
+                (n.href === "/#services" && pathname === "/")
+
               return (
                 <li key={n.href}>
-                  <Link href={n.href} scroll={true} className={`text-slate-300 hover:text-white ${isActive ? 'text-white underline' : ''}`}>
+                  <Link
+                    href={n.href}
+                    scroll={true}
+                    className={`text-slate-300 hover:text-white ${
+                      isActive ? "text-white underline" : ""
+                    }`}
+                  >
                     {n.label}
                   </Link>
                 </li>
